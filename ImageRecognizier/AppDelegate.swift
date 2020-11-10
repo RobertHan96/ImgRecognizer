8140b1ebc18d3d9ba9e1af383f8288c9d1f4ea8d
RobertHan96 <yshan4329@gmail.com>
2020-11-10 16:49:33 +0900

    런치스크린 추가, 앱디스플레이 이름 변경

--- ImageRecognizier/AppDelegate.swift
+++ ImageRecognizier/AppDelegate.swift
@@ -5,8 +5,8 @@ import Firebase
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
         FirebaseApp.configure()
+        sleep(2)
         return true
     }
 }
